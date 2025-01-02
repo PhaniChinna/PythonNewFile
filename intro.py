50a797986051899b623a2484f790ba830b4d2ddd
PhaniChinna <phaniyarram@gmail.com>
2025-01-02 11:28:54 +0000

    first commit

---
+++ intro.py
@@ -0,0 +1,67 @@
+
+# Breath_of_Triangle = int(input("Enter a Breath of Triangle: "))
+# height_of_Triangle = int(input("Enter a height of Triangle: "))
+# Area_of_Triangle = 1/2 * Breath_of_Triangle * height_of_Triangle
+# print(Area_of_Triangle)
+
+# Parallelogram  b * h
+
+# Breath_of_Parallelogram = int(input("Enter a Breath Number: "))
+# Height_of_Parallelogram = int(input("Enter a Height Number: "))
+# Area_of_Parallellogram = Breath_of_Parallelogram * Height_of_Parallelogram
+# print(Area_of_Parallellogram)
+
+# Trapezoid 1/2* (a + b) * h
+
+# Area_of_Trapezoid = int(input("Enter a Area Number : "))
+# Breath_of_Trapezoid = int(input("Enter a Breath Number :"))
+# Height_of_Trapezoid = int(input("Enter a Height Number : "))
+# Result_of_Trapezoid = 1/2 * \
+#     (Area_of_Trapezoid + Breath_of_Trapezoid) * Height_of_Trapezoid
+# print(Result_of_Trapezoid)
+
+# if Condition :
+# A = int(input("Enter a Number A : "))
+# B = int(input("Enter a Number B : "))
+# if (A > B):
+#     print(A - B)
+# print(A + B)
+
+# w = "NxtWave"
+# print(w[0] + w[6])
+
+# Word = input("Enter a String Input : ")
+# length_of_String = len(Word) - 2
+# First_Character = Word[0]
+# Last_Character = Word[len(Word) - 1]
+# Stars = "*" * length_of_String
+# print(First_Character + Stars + Last_Character)
+
+# W = "Practice"
+# length = len(W)
+# result = (length % 2)
+# print(result)
+
+
+# if(False):
+#     print("Hi")
+# else:
+#     if(False):
+#         print("Hlo")
+#     print("Hello Welcome")
+# print("Bye")
+
+
+# Num_One = int(input("Enter First Number: "))
+# Num_Two = int(input("Enter Second Number: "))
+# Num_Three = int(input("Enter Third Number: "))
+# is_Greater_A = (Num_One > Num_Two) and (Num_Two > Num_Three)
+
+# if(is_Greater_A):
+#     print(Num_One)
+# else:
+#     is_Greater_B = Num_Two > Num_Three
+#     if(is_Greater_B):
+#         print(Num_Two)
+#     else:
+#         print(Num_Three)
